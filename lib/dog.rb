9d34b15e0ced9192e5bc641daad2e1d07e96a2cf
courserav <lvsmlit@gmail.com>
2020-12-20 14:27:50 -0500

    commit tree d3965419e73bf47f280533508dbd1d58156c2d15

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+
+class Dog
+
+    @@all = []
+
+    def initialize(name = none)
+        @name = name
+        return self.save
+    end
+
+    def self.all
+        return @@all
+    end
+
+    def self.clear_all
+        return @@all.clear
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def name 
+        @name
+    end
+
+    def save
+        @@all << self
+    end
+end
